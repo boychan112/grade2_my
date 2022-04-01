81a6b0679d58d03973fd53cd43b9a72635d3b437
won_chan <83020544+boychan112@users.noreply.github.com>
2022-04-01 09:48:01 +0900

    Add files via upload

---
+++ [자료] 22-03-14/test.py
@@ -0,0 +1,3 @@
+a = int(input("내 나이는 : ")) + 100
+print("내 나이는",a,"살 입니다")
+print(type(a))
---
+++ [자료] 22-03-21/1.py
@@ -0,0 +1,35 @@
+# print("나는 이원찬이다")
+
+# a=int(input("내 나이는? : "))
+
+# print("현재 나이는 : ",a)
+# print("10년 후 나이는 : ",a+10)
+
+# num = int(input("숫자입력 : "))
+
+# if(num>0):
+#     print("양수입니다")
+# elif(num<0):
+#     print("음수입니다")
+# else:
+#     print("0일겁니다")
+
+# age=int(input("내 나이는? : "))
+
+# if(age>19):
+#     print("성인입니다")
+# else:
+#     print("미성년자입니다")
+
+a=int(input("숫자를 입력하세요 : "))
+b=int(input("숫자를 입력하세요 : "))
+c=int(input("숫자를 입력하세요 : "))
+
+if(a>b):
+    if(a>c) : print("가장 큰 수는",a,"입니다")
+
+if(b>a):
+    if(b>c) : print("가장 큰 수는",b,"입니다")
+
+if(c>a):
+    if(c>b) : print("가장 큰 수는",c,"입니다")
