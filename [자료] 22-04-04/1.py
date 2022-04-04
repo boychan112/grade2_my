0bc6365b202aea24a3541e43910722ddd3867aeb
won_chan <83020544+boychan112@users.noreply.github.com>
2022-04-04 11:30:54 +0900

    Add files via upload

---
+++ [자료] 22-04-04/1.py
@@ -0,0 +1,20 @@
+# for i in range(1,10,2):
+#     print(i)
+
+# for i in range(1,10):
+#     print(i*4)
+
+# sum=0
+# for i in range(1,21):
+#     sum+=i
+# print(sum)
+
+# for i in range(2,10):
+#     for a in range(1,10):
+#         print(i,"*",a,"=",a*i)
+
+print("안녕하세요 ", end="") #한줄에 출력하고 싶을때
+print("반갑습니다")
+
+for i in range(1,101):
+    print(i ,end=" ")
